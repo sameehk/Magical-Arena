ae567c0580755fda92edd41166dd5e78f9c6a5d1
Sameeh <sameehk33@gmail.com>
2024-07-26 00:17:14 +0530

    attribute declared

---
+++ Player.java
@@ -0,0 +1,12 @@
+
+public class Player{
+
+
+
+    public Player(int health, int strength,int attack){
+        this.health=health;
+        this.strength=strength;
+        this.attack=attack;
+        
+    }
+}
